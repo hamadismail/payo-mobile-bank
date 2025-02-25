397aace36e1e8af1878677dd0efc28ab202a3115
hamadismail <hamad.ismail.gub@gmail.com>
2025-02-25 14:28:40 +0600

    added-cashout-funtionality

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,11 +1,13 @@
 const btnAddMoney = document.getElementById('btn-add-money');
+const btnCashout = document.getElementById('btn-cashout');
+const balance = document.getElementById('balance').innerText;
 
+// Add Money
 btnAddMoney.addEventListener('click', e => {
   e.preventDefault();
-  const balance = document.getElementById('balance').innerText;
   const accountNumber = document.getElementById('account-number').value;
-  const addAmount = document.getElementById('add-amount').value;
   const accountPin = document.getElementById('account-pin').value;
+  const addAmount = document.getElementById('add-amount').value;
 
   if (addAmount > 0) {
     if (accountNumber.length === 11) {
@@ -20,6 +22,25 @@ btnAddMoney.addEventListener('click', e => {
     }
   } else {
     alert('Amount should be greater then zero');
+  }
+});
+
+// Cash Out
+let sub = parseInt(balance);
+btnCashout.addEventListener('click', e => {
+  e.preventDefault();
+  const accountNumber = document.getElementById('cashout-account-number').value;
+  const accountPin = document.getElementById('cashout-account-pin').value;
+  const cashoutAmount = document.getElementById('cashout-amount').value;
+
+  sub -= parseInt(cashoutAmount);
+
+  if (cashoutAmount <= 0) {
+    alert('Please Enter Valid Amount');
+  } else if (sub >= 0) {
+    document.getElementById('balance').innerText = sub;
+  } else {
+    alert("You didn't have enough balance");
   }
 });
 
