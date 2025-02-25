dcd921d4694e457cd6a54c7fe0482db0f907f184
hamadismail <hamad.ismail.gub@gmail.com>
2025-02-25 08:40:04 +0600

    handle-login-and-redirect-home-page

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,13 @@
 const btnLogin = document.getElementById('btn-login');
 btnLogin.addEventListener('click', e => {
   e.preventDefault();
+
+  const accountNumber = document.getElementById('account-number').value;
+  const accountPin = document.getElementById('account-pin').value;
+
+  if (accountNumber.length === 11 && accountPin === '1234') {
+    window.location.href = '../src/main.html';
+  } else {
+    alert('Invalid Credentials');
+  }
 });
